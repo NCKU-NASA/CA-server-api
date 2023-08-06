72a875349a29cf2378a16c694ff5f96ab3c383c7
Chumy <57281249+Jimmy01240397@users.noreply.github.com>
2023-08-07 00:22:48 +0800

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -5,7 +5,7 @@ sudo systemctl disable caserverapi.service
 
 sudo rm /etc/systemd/system/caserverapi.service
 
-for filename in requirements.txt main.py conf sign config.yaml venv
+for filename in requirements.txt main.py conf sign config.yaml venv .gitignore __pycache__
 do
 	sudo rm -r /etc/caserverapi/$filename
 done
@@ -14,6 +14,7 @@ sudo rm /usr/local/bin/caserverapi
 
 sudo mv /etc/caserverapi/server.key .
 sudo mv /etc/caserverapi/server.crt .
+sudo mv /etc/caserverapi/pki .
 
 if [ "`ls /etc/caserverapi`" = "" ]
 then
@@ -24,7 +25,7 @@ echo ""
 echo ""
 echo "CA Server API Service remove.sh complete."
 
-for filename in server.key server.crt
+for filename in server.key server.crt pki
 do
 	echo "Your ${filename} is at $(pwd)/${filename}."
 done
